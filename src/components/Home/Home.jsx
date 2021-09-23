501592b5518278750b242b2de3b9c1a1c9fa9f73
Tamara <tamaragmartin@gmail.com>
2021-09-23 23:05:09 +0200

    home view configured

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
 
+import { Link } from 'react-router-dom';
+
 class Home extends Component {
   render() {
-    return <div>Home</div>;
+    return (
+      <>
+      <h4>Bienvenidos a mi app</h4>
+            
+      <button><Link to="/todo">To-Do</Link></button>
+      <button><Link to="/weather">Weather</Link></button>
+      </>
+    )
   }
 }
 
